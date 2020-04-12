2164edc9fe059955789d0e00c3993d0b835271b6
Ricardo Gaya <rrgaya@gmail.com>
2020-04-12 01:42:55 -0300

    Added main function in project

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from strategy.order import Order
 from strategy.calcute_shipping import CalculateShipping
 
 
-order = Order(500)
-calculate_shipping = CalculateShipping()
+
+def main():
+    order = Order(500)
+    calculate_shipping = CalculateShipping()
+    try:
+        calculate_shipping.execute_calculation(order, Default())
+        calculate_shipping.execute_calculation(order, Express())
+    except ValueError as e:
+        print(f"Erro ao tentar calcular o frete - {e}")
 
 
-calculate_shipping.execute_calculation(order, Default())
-calculate_shipping.execute_calculation(order, Express())
+if __name__ == "__main__":
+    main()
